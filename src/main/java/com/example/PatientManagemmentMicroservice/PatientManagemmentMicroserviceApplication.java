75f38e89d5be4574f70c2c3daf72337e01c8ee71
Mehul Jain <mehuljain@Mehuls-MacBook-Pro.local>
2025-09-26 18:14:00 +0530

    setting up

---
+++ src/main/java/com/example/PatientManagemmentMicroservice/PatientManagemmentMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.example.PatientManagemmentMicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PatientManagemmentMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PatientManagemmentMicroserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/PatientManagemmentMicroservice/PatientManagemmentMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.PatientManagemmentMicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PatientManagemmentMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
